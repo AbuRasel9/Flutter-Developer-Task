c191fdc08d9751b2980c1ac04eba2b3c51f66c9d
Abu Rasel <aburaselahamed99@gmail.com>
2025-08-03 12:03:26 +0600

    form model form provider update

--- lib/model/formModel/form_model.dart
+++ lib/model/formModel/form_model.dart
@@ -1,38 +1,101 @@
-// To parse this JSON data, do
-//
-//     final formModel = formModelFromJson(jsonString);
-
-import 'dart:convert';
-
-import 'package:bloc_clean_architecture/model/formModel/section.dart';
-
-FormModel formModelFromJson(String str) => FormModel.fromJson(json.decode(str));
-
-String formModelToJson(FormModel data) => json.encode(data.toJson());
-
 class FormModel {
-  String? formName;
-  int? id;
-  List<Section>? sections;
+  final String formName;
+  final int id;
+  final List<FormSection> sections;
 
   FormModel({
-    this.formName,
-    this.id,
-    this.sections,
+    required this.formName,
+    required this.id,
+    required this.sections,
   });
 
-  factory FormModel.fromJson(Map<String, dynamic> json) => FormModel(
-    formName: json["formName"],
-    id: json["id"],
-    sections: json["sections"] == null ? [] : List<Section>.from(json["sections"]!.map((x) => Section.fromJson(x))),
-  );
-
-  Map<String, dynamic> toJson() => {
-    "formName": formName,
-    "id": id,
-    "sections": sections == null ? [] : List<dynamic>.from(sections!.map((x) => x.toJson())),
-  };
+  factory FormModel.fromJson(Map<String, dynamic> json) {
+    return FormModel(
+      formName: json['formName'],
+      id: json['id'],
+      sections: (json['sections'] as List)
+          .map((section) => FormSection.fromJson(section))
+          .toList(),
+    );
+  }
 }
 
+class FormSection {
+  final String name;
+  final String key;
+  final List<FormField> fields;
 
+  FormSection({
+    required this.name,
+    required this.key,
+    required this.fields,
+  });
 
+  factory FormSection.fromJson(Map<String, dynamic> json) {
+    return FormSection(
+      name: json['name'],
+      key: json['key'],
+      fields: (json['fields'] as List)
+          .map((field) => FormField.fromJson(field))
+          .toList(),
+    );
+  }
+}
+
+class FormField {
+  final int id;
+  final String key;
+  final FieldProperties properties;
+
+  FormField({
+    required this.id,
+    required this.key,
+    required this.properties,
+  });
+
+  factory FormField.fromJson(Map<String, dynamic> json) {
+    return FormField(
+      id: json['id'],
+      key: json['key'],
+      properties: FieldProperties.fromJson(json['properties']),
+    );
+  }
+}
+
+class FieldProperties {
+  final String type;
+  final String defaultValue;
+  final String hintText;
+  final int minLength;
+  final int maxLength;
+  final String label;
+  final String listItems;
+  final bool multiSelect;
+  final bool multiImage;
+
+  FieldProperties({
+    required this.type,
+    required this.defaultValue,
+    required this.hintText,
+    required this.minLength,
+    required this.maxLength,
+    required this.label,
+    this.listItems = '',
+    this.multiSelect = false,
+    this.multiImage = false,
+  });
+
+  factory FieldProperties.fromJson(Map<String, dynamic> json) {
+    return FieldProperties(
+      type: json['type'],
+      defaultValue: json['defaultValue'] ?? '',
+      hintText: json['hintText'] ?? '',
+      minLength: json['minLength'] ?? 0,
+      maxLength: json['maxLength'] ?? 255,
+      label: json['label'],
+      listItems: json['listItems'] ?? '',
+      multiSelect: json['multiSelect'] ?? false,
+      multiImage: json['multiImage'] ?? false,
+    );
+  }
+}
--- lib/provider/from_provider.dart
+++ lib/provider/from_provider.dart
@@ -1,27 +1,54 @@
-import 'package:bloc_clean_architecture/model/formModel/form_model.dart';
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../model/formModel/form_model.dart';
 
-import '../repository/form_repository.dart';
+class FormProvider with ChangeNotifier {
+  List<FormModel> _forms = [];
+  FormModel? _currentForm;
+  Map<String, dynamic> _formData = {};
+  Map<String, dynamic> _submissionData = {};
 
-class FormProvider extends ChangeNotifier{
-  final FormRepository  _repository=FormRepository();
-  List<FormModel>_forms=[];
+  List<FormModel> get forms => _forms;
+  FormModel? get currentForm => _currentForm;
+  Map<String, dynamic> get formData => _formData;
+  Map<String, dynamic> get submissionData => _submissionData;
 
-  //get data
-  List<FormModel> get fromData=>_forms;
+  Future<void> loadForms() async {
+    try {
+      final form1 = await rootBundle.loadString('assets/form_1.json');
+      final form2 = await rootBundle.loadString('assets/form_2.json');
+      final form3 = await rootBundle.loadString('assets/form_3.json');
 
-  Map<String,dynamic>formValues={};
-  ///fetch from data
-  Future<void>fetchFormData()async{
-    _forms=await _repository.fetchForms();
+      _forms = [
+        FormModel.fromJson(json.decode(form1)),
+        FormModel.fromJson(json.decode(form2)),
+        FormModel.fromJson(json.decode(form3)),
+      ];
+      notifyListeners();
+    } catch (e) {
+      debugPrint('Error loading forms: $e');
+    }
+  }
+
+  void setCurrentForm(FormModel form) {
+    _currentForm = form;
+    _formData = {};
     notifyListeners();
   }
-  void updateFeildValue(String key,dynamic value){
-    formValues[key]=value;
+
+  void updateFormData(String key, dynamic value) {
+    _formData[key] = value;
     notifyListeners();
   }
 
-  Map<String,dynamic>getSubmissionData(){
-    return formValues;
+  void submitForm() {
+    _submissionData = Map.from(_formData);
+    notifyListeners();
+  }
+
+  void clearSubmission() {
+    _submissionData = {};
+    notifyListeners();
   }
 }
